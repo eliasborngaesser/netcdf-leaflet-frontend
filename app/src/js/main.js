95d1452d23401378929645be6462d259bafb3979
EliasBorngaesser <elias.borngaesser@gmail.com>
2020-03-23 23:32:51 +0100

    moved index.html

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -5,18 +5,17 @@ function initMap() {
         maxZoom: 18,
         
         timeDimension: true,
-        timeDimensionOptions: {
-            timeInterval: "2018-07-06T05:00:00Z/PT30H",//hours minus 1
-            period: 'PT60M'
-        },
+        timeDimensionOptions: get_timeDimensionOptions(),
         timeDimensionControl: true,
 
     });
     L.control.scale().addTo(map)
-    map.setView(['49.99286', '8.24725'], 15)
+    map.timeDimension.setCurrentTime(0);
+    map.setView(get_Location(), 15)
 
     var baseLayers = getCommonBaseLayers(map); //from baselayers js
     var BasicControl =L.control.groupedLayers(baseLayers, groupedOverlays,controlOptions).addTo(map);
+    L.control.slideMenu(projectHandling).addTo(map);
 
     map.on('overlayadd', function(eventLayer) {
         if (layerMapping.has(eventLayer.name)){
@@ -69,3 +68,22 @@ var mapStuff = initMap();
 var map = mapStuff.map;
 //Changing Default Cursor
 $('.leaflet-container').css('cursor','crosshair');
+$(function () {
+    setNavigation();
+});
+
+function setNavigation() {
+    var path = window.location.pathname;
+    path = path.replace(/\/$/, "");
+    path = decodeURIComponent(path);
+    path_array = path.split('/');
+    project= path_array[path_array.length-2];
+
+    $(".sidenav a").each(function () {
+        href_array=$(this).attr('href').split('/');
+        href= href_array[href_array.length-2];
+        if (project === href) {
+            $(this).closest('a').addClass('w3-green');
+        }
+    });
+}
