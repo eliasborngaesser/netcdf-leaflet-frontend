79c6088f1342e2ab1d69d06f6d55f7b894f55ec3
EliasBorngaesser <elias.borngaesser@gmail.com>
2020-04-12 00:20:00 +0200

    added OpenTopoMap

--- app/src/js/baselayers.js
+++ app/src/js/baselayers.js
@@ -5,8 +5,13 @@ function getCommonBaseLayers(map){
     var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     });
+    var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	maxZoom: 17,
+	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
     osmLayer.addTo(map);
     return {
-        "OSM": osmLayer
+        "OSM": osmLayer,
+        "OpenTopoMap": OpenTopoMap
     };
 }
